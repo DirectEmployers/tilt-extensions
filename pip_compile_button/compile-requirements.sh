cfeb9a1e2a61706a67d74b5e9f63219c849e7a25
Tim Loyer <tim.loyer@gmail.com>
2022-03-04 09:30:10 -0500

    fix python syntax in bash script, remove package hashing

--- pip_compile_button/compile-requirements.sh
+++ pip_compile_button/compile-requirements.sh
@@ -4,23 +4,23 @@ set -e
 
 exec_path=$1
 requirements=$2
-dev_requirements=${3-false}
+dev_requirements=${3-""}
 
 compile_in()
 {
   exec_path=$1
-  requirements_input=$2
-  requirements_output=${requirements_input//.in/.txt}
+  input=$2
+  output=${input//.in/.txt}
 
-  printf "Compiling ${requirements_input} -> ${requirements_output} ...\n"
-  kubectl exec "${exec_path}" -- \
-    pip-compile --generate-hashes "${requirements_input}"
+  echo "Compiling ${input} -> ${output} ..."
+  kubectl exec "${exec_path}" -- pip-compile --quiet "${hash}" "${input}"
 
-  printf "Collect files locally...\n"
-  kubectl exec "${exec_path}" -- \
-    cat "${requirements_output}" > "${requirements_output}"
+  echo "Collect ${output} file locally..."
+  kubectl exec "${exec_path}" -- cat "${output}" > "${output}"
 }
 
 compile_in "${exec_path}" "${requirements}"
-if dev_requirements:
+if [[ $dev_requirements != "" ]]; then
+  echo
   compile_in "${exec_path}" "${dev_requirements}"
+fi
