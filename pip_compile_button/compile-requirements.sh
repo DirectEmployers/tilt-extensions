18688f9fee7737f894bdc5a79b031173572ec0fb
Tim Loyer <tim.loyer@gmail.com>
2022-03-09 12:04:26 -0500

    add comments to compile script

--- pip_compile_button/compile-requirements.sh
+++ pip_compile_button/compile-requirements.sh
@@ -5,6 +5,8 @@ set -e
 POSITIONAL_ARGS=()
 COMPILE_ARGS=()
 
+# Based on example from:
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 while [[ $# -gt 0 ]]; do
   case $1 in
     -*|--*)
@@ -28,14 +30,22 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}"
 
+# Get kubectl exec resource name or path.
 exec_path=$1
+
+# Get local destination path for compiled requirements files.
 destination=$2
+
+# Shift previous two arguments and treat remaining as requirements file names.
 shift
 shift
 requirements=$@
 
 compile_in()
 {
+  # Compile a list of requirements.in files using kubectl.
+  # Additional pip-compile arguments are passed in via $compile_args.
+
   exec_path=$1
   compile_args=$2
   shift
@@ -43,8 +53,12 @@ compile_in()
   inputs=("$@")
 
   for input in "${inputs[@]}"; do
+    # Get filename only.
     input_name=$(basename $input)
+
+    # Update .in to .txt for compiled filename.
     output_name="${input_name//.in/.txt}"
+
     echo
     echo "Compiling ${input_name} → ${output_name}..."
     kubectl exec "${exec_path}" -- pip-compile ${compile_args[@]} "${input}"
@@ -53,6 +67,9 @@ compile_in()
 
 collect_txt()
 {
+  # Copy the contents of compiled requirements files back out
+  # to local destination path ($compiled_dest).
+
   exec_path=$1
   compiled_dest=$2
   shift
@@ -60,15 +77,22 @@ collect_txt()
   inputs=("$@")
 
   for input in "${inputs[@]}"; do
+    # Update .in to .txt for compiled file path.
     output=${input//.in/.txt}
+
+    # Get filename only.
     o_name=$(basename $output)
+
+    # Construct compiled file output path.
     o_dest="${compiled_dest}/${o_name}"
+
     echo
     echo "Collect ${o_name} file locally..."
     kubectl exec "${exec_path}" -- cat "${output}" > "${o_dest}"
   done
 }
 
+# Compile and then save results to local filesystem.
 compile_in "${exec_path}" "${COMPILE_ARGS}" ${requirements[@]}
 collect_txt "${exec_path}" "${destination}" ${requirements[@]}
 echo
