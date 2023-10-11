f96d07eb5f0a9612e1511995324872d4187a5c66
Tim Loyer <tloyer@directemployers.org>
2023-10-11 09:22:52 -0400

    add tarfetch script

---
+++ tarfetch/scripts/tarfetch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -eux
+
+get_tar() {
+  kubectl exec -i -n "$TARFETCH_NAMESPACE" "$TARFETCH_RESOURCE_NAME" -- \
+    tar -c -f - --atime-preserve=system --directory="$TARFETCH_SRC_DIR" "$TARFETCH_EXCLUDE" .
+}
+
+unpack() {
+  tar -x -f - "$TARFETCH_VERBOSE" "$TARFETCH_KEEP_NEWER" --directory="$TARFETCH_TARGET_DIR"
+}
+
+get_tar | unpack
+
+echo '[tarfetch] Done: Sync from container has finished.'
