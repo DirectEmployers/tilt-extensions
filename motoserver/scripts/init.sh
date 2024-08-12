544c590eb336d3899c9529578bbd3a46db3fe41f
Tim Loyer <tloyer@directemployers.org>
2024-08-12 17:24:58 -0400

    add statefulset for init script, basic test tiltfile, init script stub

---
+++ motoserver/scripts/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+echo "=============================================================================================="
+echo "This is a placeholder for a MotoServer init script. See the extension readme for more details!"
+echo "URL: https://github.com/DirectEmployers/tilt-extensions/blob/main/motoserver/README.md"
+echo "=============================================================================================="
